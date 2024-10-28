484bfdbf71a2cc3af579b371089ce85bb1dd5e3b
Dhruva Srinivas <dhruvas17068@gmail.com>
2024-10-27 19:37:46 -0700

    frontend

--- web/script.js
+++
@@ -1,6 +0,0 @@
-// fetch from localhost:5000 and display the data
-let url = 'http://localhost:5000';
-fetch(url).then(response => response.json()).then(data => {
-    console.log(data);
-    document.getElementById('data').innerHTML = JSON.stringify(data);
-})
